ea8295bf2a8b3bc2a3a54a80c6fa19e58ddf9c0f
Akadem <kostianetskyi@gmail.com>
2021-03-30 11:48:30 +0300

    try to fix handling network error

--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,8 @@ const getData = (node, name) => (node.querySelector(name).textContent);
 export default (xml) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(xml, 'text/xml');
-    console.log(doc.documentElement.textContent);
+    // console.log(doc.documentElement.textContent);
+    console.log(doc.documentElement);
     const channel = doc.querySelector('channel');
     const items = Array.from(doc.querySelectorAll('item'));
 
