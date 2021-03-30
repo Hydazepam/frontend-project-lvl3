e7fd7f4a5df66fc50bd5ac6193bbc72460151f81
Akadem <kostianetskyi@gmail.com>
2021-03-30 14:41:18 +0300

    try to fix handling network error

--- src/parser.js
+++ src/parser.js
@@ -3,12 +3,13 @@ const getData = (node, name) => (node.querySelector(name).textContent);
 export default (xml) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(xml, 'text/xml');
-console.log(doc.querySelector('parsererror'));
+    console.log(doc);
+// console.log(doc.querySelector('parsererror'));
     if (doc.querySelector('parsererror') !== null) {
-        throw Error('parserError');
-    } if (doc.querySelector('parsererror') === null) {
-        throw Error('networkError')
-    }
+        throw Error('parserError');}
+    // } if (doc.querySelector('parsererror') === null) {
+    //     throw Error('networkError')
+    // }
     // const isParseError = doc.querySelector('parsererror') !== null;
     // if (isParseError) throw Error('parse xml error');
 
