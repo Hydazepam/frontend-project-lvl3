11635a02d52a4dd137d5a4bdc14ed22df81e6115
Akadem <kostianetskyi@gmail.com>
2021-04-06 15:11:32 +0300

    try to fix modal

--- src/app.js
+++ src/app.js
@@ -4,89 +4,80 @@ import { setLocale } from 'yup';
 import fetchData from './updater';
 
 const validate = (link, links) => {
-    setLocale({
-        string: {
-            url: 'notURL',
-        },
-        mixed: {
-            notOneOf: 'duplicate',
-        }
-    });
+  setLocale({
+    string: {
+      url: 'notURL',
+    },
+    mixed: {
+      notOneOf: 'duplicate',
+    },
+  });
 
-    let schema = yup.string()
-        .url()
-        .notOneOf(links);
-    try {
-        schema.validateSync(link);
-        return null;
-    } catch (err) {
-        return err.message;
-    };
+  const schema = yup.string()
+    .url()
+    .notOneOf(links);
+  try {
+    schema.validateSync(link);
+    return null;
+  } catch (err) {
+  return err.message;
+  }
 };
 
 export default (i18next) => {
-    const state = {
-        form: {
-            state: 'empty',
-            field: {
-                valid: true,
-                error: null,
-              },
-        },
-        requestState: {
-            valid: true,
-            error: null,
-        },
-        data: {
-            feeds: [],
-            posts: [],
-        },
-        viewedPosts: [],
+  const state = {
+    form: {
+      state: 'empty',
+      field: {
+        valid: true,
         error: null,
+      },
+    },
+    requestState: {
+      valid: true,
+      error: null,
+    },
+    data: {
+      feeds: [],
+      posts: [],
+    },
+    viewedPosts: [],
+    error: null,
+  };
+
+  const watchedState = view(state, i18next);
+  const form = document.querySelector('.rss-form');
+  const input = document.querySelector('input');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const links = state.data.feeds.map((feed) => feed.link);
+    const link = input.value;
+
+    const error = validate(link, links);
+    if (error) {
+      watchedState.form.field = {
+        valid: false,
+        error,
+      };
+      return;
     };
-    
-    const watchedState = view(state, i18next);
-    const form = document.querySelector('.rss-form');
-    const input = document.querySelector('input');
 
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
+    watchedState.form.field = {
+      valid: true,
+      error: null,
+    };
+    watchedState.form.state = 'sent';
 
-        const links = state.data.feeds.map((feed) => feed.link);
-        const link = input.value;
+    fetchData(link, watchedState);
 
-        const error = validate(link, links);
-        if (error) {
-            watchedState.form.field = {
-                valid: false,
-                error,
-            };
-            return;
-        };
+    input.addEventListener('input', () => {
+      watchedState.form.state = 'empty';
+      watchedState.form.error = null;
 
-        watchedState.form.field = {
-            valid: true,
-            error: null,
-        };
-        watchedState.form.state = 'sent';
-
-        fetchData(link, watchedState);
-
-        input.addEventListener('input', () => {
-            watchedState.form.state = 'empty';
-            watchedState.form.error = null;
-
-            watchedState.requestState.valid = true;
-            watchedState.requestState.error = null;
-        });
+      watchedState.requestState.valid = true;
+      watchedState.requestState.error = null;
     });
-
-    // const posts = document.querySelector('.posts');
-    // posts.addEventListener('click', (e) => {
-    //     const clickedPostId = e.target.dataset.id;
-    //     watchedState.viewedPosts.push(clickedPostId);
-    // });
-
-    // const modal = document.querySelector('#myModal');
-    // modal.addEventListener('show.bs.modal', (e) => handleModal(e, watchedState)); // new lines
+  });
 };
